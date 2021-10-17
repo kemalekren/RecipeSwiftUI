dfc720905a402c720e79f61f10ed2afb79837d3b
Esat Kemal Ekren <kemalekren@hotmail.com>
2021-10-17 20:30:47 +0300

    Initial Commit

---
+++ RecipeProject/RecipeProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeProjectApp.swift
+//  RecipeProject
+//
+//  Created by Kemal Ekren on 17.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct RecipeProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
